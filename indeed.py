91c0c548f6711a27b6ef6734a855092a06a33c70
Deependra Singh <deependra578@gmail.com>
2019-07-06 18:24:40 +0530

    Done the changes in indeed file

--- indeed.py
+++ indeed.py
@@ -1,9 +1,10 @@
 import scrapy
 from bs4 import BeautifulSoup
 import sys
+import re
 
-class QuotesSpider(scrapy.Spider):
-    name = 'indeed-jobs'
+class IndeedSpider(scrapy.Spider):
+    name = 'indeed'
     start_urls = [
         'https://www.indeed.co.in/jobs?q=software+developer&l=Malad%2C+Mumbai%2C+Maharashtra',
     ]
@@ -11,11 +12,19 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         for quote in response.css('div.jobsearch-SerpJobCard'):
             yield {
-                'orgname': BeautifulSoup(quote.css('span.company::text').get(),'lxml').text ,
-                'location': quote.css('div.location::text').get() ,
+                'jobtitle':self.removehtml_tags(quote.css('a.jobtitle').get()),
+                'orgname': self.removehtml_tags(quote.css('span.company').get())  ,
+                'location': self.removehtml_tags(quote.css('div.location').get()) ,
             }
 
         next_page = response.css('div.pagination a:last-child::attr("href")').get()
         print(next_page)
         if next_page is not None:
             yield response.follow(next_page, self.parse)
+
+    def removehtml_tags(self,text):
+        try:
+            clean = re.compile('<.*?>')
+            return re.sub(clean, '', text) 
+        except:
+            return ""
